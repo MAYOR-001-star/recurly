0c73540cebedbfe69f72b24d60bdff733cb86cf5
MAYOR-001-star <mayorkunabdulazeez01@gmail.com>
2026-09-07 03:12:36 +0100

    implement fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,34 @@
-import { Stack } from "expo-router";
+import "@/global.css"
+import {useFonts} from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import {useEffect} from 'react';
+import {Stack} from "expo-router";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+    const [loaded, error] = useFonts({
+        'sans-regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+        'sans-bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+        'sans-medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
+        'sans-semibold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
+        'sans-extrabold': require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
+        'sans-light': require('../assets/fonts/PlusJakartaSans-Light.ttf')
+    });
+
+    useEffect(() => {
+        if (loaded || error) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded, error]);
+
+    if (!loaded && !error) {
+        return null;
+    }
+
     return (
-        <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="(tabs)" />
+        <Stack screenOptions={{headerShown: false}}>
+            <Stack.Screen name="(tabs)"/>
         </Stack>
-    );
+    )
 }
